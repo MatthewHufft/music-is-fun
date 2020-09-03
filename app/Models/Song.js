333266d9de811a47cf57ca9f98cae1a5425e69cb
MatthewHufft <matthufft777@gmail.com>
2020-09-03 17:50:15 -0600

    Fully styled and displayed cool

--- app/Models/Song.js
+++ app/Models/Song.js
@@ -16,10 +16,10 @@ export default class Song {
 
   get Template() {
     if (this.sandboxSong) {
-      return `   <li class="list-group-item" role= "button" onclick= "app.songsController._viewMySong('${this._id}')" ><img class = "searchRIMG" src="${this.albumArt}"> ${this.artist}, ${this.album}, ${this.title}, Price: $${this.price}</li>`
+      return `   <li class="list-group-item grow mb-2 shadow rounded" role= "button" onclick= "app.songsController._viewMySong('${this._id}')" ><img class = "searchRIMG" src="${this.albumArt}"> ${this.artist}, ${this.album}, ${this.title}, Price: $${this.price}</li>`
     }
     return `
-    <li class="list-group-item" role= "button" onclick= "app.songsController._viewSong('${this._id}')" ><img class = "searchRIMG" src="${this.albumArt}"> ${this.artist}, ${this.album}, ${this.title}, Price: $${this.price}</li>
+    <li class="list-group-item grow mb-2 shadow rounded" role= "button" onclick= "app.songsController._viewSong('${this._id}')" ><img class = "searchRIMG" src="${this.albumArt}"> ${this.artist}, ${this.album}, ${this.title}, Price: $${this.price}</li>
     `;
   }
 
@@ -30,7 +30,7 @@ export default class Song {
   }
 
   get currentSongTemplate() {
-    return `<div class="card">
+    return `<div class="card shadow" style="width: 30rem; ">
     <img src="${this.albumArt}" class="card-img-top" alt="...">
     <div class="card-body text-center">
       <h5 class="card-title">${this.artist}, ${this.title}</h5>
@@ -58,42 +58,3 @@ export default class Song {
 
 
 }
-
-
-// {resultCount: 50, results: Array(50)}
-// resultCount: 50
-// results: Array(50)
-// 0:
-// artistId: 2900850
-// artistName: "J-Live"
-// artistViewUrl: "https://music.apple.com/us/artist/j-live/2900850?uo=4"
-// artworkUrl30: "https://is3-ssl.mzstatic.com/image/thumb/Music/v4/4e/f4/5b/4ef45b85-237c-6d0c-3560-16be10f5ce6a/source/30x30bb.jpg"
-// artworkUrl60: "https://is3-ssl.mzstatic.com/image/thumb/Music/v4/4e/f4/5b/4ef45b85-237c-6d0c-3560-16be10f5ce6a/source/60x60bb.jpg"
-// artworkUrl100: "https://is3-ssl.mzstatic.com/image/thumb/Music/v4/4e/f4/5b/4ef45b85-237c-6d0c-3560-16be10f5ce6a/source/100x100bb.jpg"
-// collectionCensoredName: "The Hear After"
-// collectionExplicitness: "explicit"
-// collectionId: 164406129
-// collectionName: "The Hear After"
-// collectionPrice: 11.99
-// collectionViewUrl: "https://music.apple.com/us/album/whoever/164406129?i=164406172&uo=4"
-// contentAdvisoryRating: "Explicit"
-// country: "USA"
-// currency: "USD"
-// discCount: 1
-// discNumber: 1
-// isStreamable: true
-// kind: "song"
-// previewUrl: "https://audio-ssl.itunes.apple.com/itunes-assets/Music/17/aa/37/mzm.xnfzviin.aac.p.m4a"
-// primaryGenreName: "Hip-Hop/Rap"
-// releaseDate: "2005-08-30T12:00:00Z"
-// trackCensoredName: "Whoever"
-// trackCount: 13
-// trackExplicitness: "explicit"
-// trackId: 164406172
-// trackName: "Whoever"
-// trackNumber: 5
-// trackPrice: 0.99
-// trackTimeMillis: 276200
-// trackViewUrl: "https://music.apple.com/us/album/whoever/164406129?i=164406172&uo=4"
-// wrapperType: "track"
-// __proto__: Object
